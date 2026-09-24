538635d291d6efaf6a8887d3620bd6ca7fc7f97d
jcdelgadomo <jcdelgadomo@oaj.gob.mx>
2026-09-24 10:02:10 -0600

    patron michelin beta faltante

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import LoginGate from './pages/LoginGate.jsx'
-import BloqueadoGate from './pages/BloqueadoGate.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { ProyectoProvider } from './context/ProyectoContext.jsx'
 import { UIProvider } from './context/UIContext.jsx'
-
-const vista = new URLSearchParams(window.location.search).get('pwvista')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <ProyectoProvider>
         <UIProvider>
-          {vista === 'login' ? <LoginGate /> : vista === 'bloqueado' ? <BloqueadoGate /> : <App />}
+          <App />
         </UIProvider>
       </ProyectoProvider>
     </AuthProvider>
